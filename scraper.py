6884a9bbc7416fae11ca6fea7c5d4ca691410e08
Josh Levy <jml268@case.edu>
2019-11-11 21:08:09 -0500

    all refunding threads

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import time
 from bs4 import BeautifulSoup
 
 url_base = 'https://www.nulled.to/topic/'
-url_extra = '783730-robomans-refunding-service-10-high-success-rates-quick-response-best-and-cheap-amazon-bestbuy-nike-many-more/'
+url_extra = '777237-★fuckkikis-high-end-refund-service★lenovo-microsoft-qvc-best-buy-google-oneplus-and-many-more★/page-4'
 response = requests.get(url_base + url_extra)
 soup = BeautifulSoup(response.text, "html.parser")
 print(soup.prettify())
