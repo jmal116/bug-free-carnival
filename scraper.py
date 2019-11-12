0faa81775b223438d8ca6491a0c8b42d37662c4b
Josh Levy <jml268@case.edu>
2019-11-11 20:38:22 -0500

    got some good threads

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import time
 from bs4 import BeautifulSoup
 
 url_base = 'https://www.nulled.to/topic/'
-url_extra = '830359-trengods-refunding-service-cheapest-rates-fast-quick-communication-worldwide/page-4'
+url_extra = '783730-robomans-refunding-service-10-high-success-rates-quick-response-best-and-cheap-amazon-bestbuy-nike-many-more/'
 response = requests.get(url_base + url_extra)
 soup = BeautifulSoup(response.text, "html.parser")
 print(soup.prettify())
