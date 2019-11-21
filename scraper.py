12aecd5adc782329a043ea51e280cf9c3af86e96
Josh Levy <jml268@case.edu>
2019-11-20 19:49:25 -0500

    10 botnet threads

--- scraper.py
+++ scraper.py
@@ -3,12 +3,12 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-url_base = 'https://www.nulled.to/topic/'
-url_extra = '515844-hurr1s-refunding-cave-10-fastest-responding-refunder-amazoncomcaukauold-orders-macysbestbuytargetnordstormwalmartnike-and-many-more/'
+url_base = 'https://hackforums.net/showthread.php?'
+url_extra = 'tid=55342'
 response = requests.get(url_base + url_extra)
 soup = BeautifulSoup(response.text, "html.parser")
 print(soup.prettify())
 
 url_extra = url_extra + 'page-1' if url_extra[-1] == '/' else url_extra
-with open(f'CaaS/{url_extra}.html', 'w+') as file:
+with open(f'crimeware/{url_extra}.html', 'w+') as file:
     file.write(soup.prettify())
